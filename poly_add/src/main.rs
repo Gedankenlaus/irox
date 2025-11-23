59d16904ffb0b0240795a1a7e2bc8df053ea38e3
Robert <posch.robert@gmail.com>
2025-11-23 20:07:12 +0100

    use closures for formatting the polynomials

--- poly_add/src/main.rs
+++ poly_add/src/main.rs
@@ -1,4 +1,5 @@
 // this will be the testing ground, use List for a polynomial adder
+
 pub mod poly;
 
 use poly::Polynomial;
@@ -6,18 +7,20 @@ use poly::Polynomial;
 fn main() {
     let x = Polynomial::new();
     let poly_series = 3.0 * x.to(2) + 1.0 * x.to(3) + 3.0 * x.to(4);
-    let mut poly_str_vec: Vec<String> = Vec::new();
 
-    for poly in poly_series.iter() {
-        let c = (*poly).coefficient;
-        let order = (*poly).order;
+    // use mapping for the formatting the poly string
+    let poly_str_vec: Vec<String> = poly_series
+        .iter()
+        .map(|x| {
+            let coeff = x.coefficient;
+            let order = x.order;
+            if coeff == 1.0 {
+                return format!("x^{order}");
+            }
 
-        if c == 1.0 {
-            poly_str_vec.push(format!("x^{order}"));
-        } else {
-            poly_str_vec.push(format!("{c}x^{order}"));
-        }
-    }
+            format!("{coeff}x^{order}")
+        })
+        .collect();
 
     let joined_poly_str = poly_str_vec.join(" + ");
 
